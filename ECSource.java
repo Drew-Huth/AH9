1e3e5b9c1b20750f9759e438376a4d04dca7c9b4
Drew-Huth <147175635+Drew-Huth@users.noreply.github.com>
2023-11-06 20:02:38 +0000

    yurrrrr

--- ECSource.java
+++ ECSource.java
@@ -10,15 +10,25 @@ class Node<T> {
 
 class ECSource {
   public static <T> boolean isUnivalueList(Node<T> head) {
-    // todo
+    while (head.next != null) {
+      if (head.val != head.next.val)
+        return false;
+      isUnivalueList(head = head.next);
+    }
     return true;
   }
 
   public static void main(String[] args) {
-        Node<String> z = new Node<>("z");
-        // z
+    Node<Integer> u = new Node<>(2);
+    Node<Integer> v = new Node<>(2);
+    Node<Integer> w = new Node<>(2);
+    Node<Integer> x = new Node<>(2);
+    Node<Integer> y = new Node<>(2);
 
-        // Printing solution
-        System.out.println((ECSource.isUnivalueList(z)));
-    }
+    u.next = v;
+    v.next = w;
+    w.next = x;
+    x.next = y;
+    System.out.println((ECSource.isUnivalueList(u)));
+  }
 }
